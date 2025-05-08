8e4c1d510299bf8b5c24abd887052bbd7581c4f8
Shivang Rastogi <shivangrastogi73@gmail.com>
2025-05-09 03:45:52 +0530

    Backend Integration

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,7 @@
 // src/config.js
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:5000";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL 
+
+// || "http://127.0.0.1:5000"
+;
 
 export default BACKEND_URL;
